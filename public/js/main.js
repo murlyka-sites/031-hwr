6393e6909142d0c9a0092af9b6c9d8d17b990862
Milok <milok.murlika@yandex.ua>
2017-12-26 08:04:53 +0200

    Сверстал попап

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,18 @@
 
 let scroll = new SmoothScroll('a[data-scroll]');
+
+$('[data-fancybox]').fancybox({
+	autoFocus: false
+})
+
+$('[data-to-popup]').click(function () {
+	$.fancybox.close();
+	$.fancybox.open([
+	{
+		src: $(this).attr('href')		
+	}],
+	{
+		autoFocus: false
+	});
+	return false;
+})
